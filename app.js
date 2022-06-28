2c8ddab5050c12e84a8da00fe2c5822513fb08d4
Bmalik73 <bmalik01@live.fr>
2022-06-28 14:33:06 +0200

    bugfix js script

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
@@ -12,7 +11,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         //2. Display the result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = `dice-${dice}.dice`;
+        diceDOM.src = `picture/dice-${dice}.png`;
 
 
        //3. Update the round score IF the rolled number was NOT a 1
@@ -94,6 +93,12 @@ function init() {
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 //var x = document.querySelector('#score-0').textContent;
+
+
+
+
+
+
 
 
 
